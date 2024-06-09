b586a22098d1ef8241f0f18b65b37e86596ff321
Coimbra <adsjkbd@gmail.com>
2024-06-09 14:17:12 -0300

    Lazy Assert Messages

--- src/test/java/math/SimpleMathTest.java
+++ src/test/java/math/SimpleMathTest.java
@@ -8,9 +8,19 @@ public class SimpleMathTest {
     @Test
     void testSum() {
         SimpleMath math = new SimpleMath();
+        /*
         Double actual = math.sum(2.5, 2.5);
         double expected = 5.0;
 
-        Assertions.assertEquals(expected, actual);
+        Assertions.assertEquals(expected, actual, "2.5 + 2.5  did not produce 5.0!");
+        */
+
+        Double a = 4.5;
+        Double b = 3.5;
+        Double actual = math.sum(a, b);
+        double expected = 8.0;
+
+        // Mensagem de erro preguiçosa usando lambda
+        Assertions.assertEquals(expected, actual, () -> String.format("%.2f + %.2f did not produce %.2f!", a, b, expected));
     }
 }
