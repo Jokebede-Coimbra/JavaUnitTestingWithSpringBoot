513ef058f3e8fef7fb8ca17020fc1a822a2d5419
Coimbra <adsjkbd@gmail.com>
2024-06-13 07:13:05 -0300

    @ParameterizedTest com @ValueSource

--- src/test/java/math/SimpleMathTestS4.java
+++ src/test/java/math/SimpleMathTestS4.java
@@ -2,10 +2,7 @@ package math;
 
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.*;
 
 import java.util.stream.Stream;
 
@@ -16,25 +13,17 @@ public class SimpleMathTestS4 {
 
     SimpleMath math;
 
-    @BeforeAll
-    static void setup() {
-        System.out.println("Running @BeforeAll method!");
-    }
-
-    @AfterAll
-    static void cleanup() {
-        System.out.println("Running @AfterAll method!");
-    }
-
     @BeforeEach
     void beforeEachMethod() {
         math = new SimpleMath();
         System.out.println("Running @BeforeEach method!");
     }
 
-    @AfterEach
-    void afterEachMethod() {
-        System.out.println("Running @AfterEach method!");
+    @ParameterizedTest
+    @ValueSource(strings = {"Senna, F1", "Pelé, Football", "Keith Moon"})
+    void testValueSource(String firsName) {
+        System.out.println(firsName);
+        assertNotNull(firsName);
     }
 
     @DisplayName("Test double division [ firstNumber,  secondNumber,  expected]")
