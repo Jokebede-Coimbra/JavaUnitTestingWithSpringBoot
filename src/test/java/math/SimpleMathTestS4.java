720aaf46743e493eb887a6b038baaae897a78e92
Coimbra <adsjkbd@gmail.com>
2024-06-13 06:00:36 -0300

    @ParameterizedTest com @CsvSource

--- src/test/java/math/SimpleMathTestS4.java
+++ src/test/java/math/SimpleMathTestS4.java
@@ -3,6 +3,7 @@ package math;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
@@ -37,25 +38,32 @@ public class SimpleMathTestS4 {
 
     @DisplayName("Test 8.0 / 2.0 = 4.0")
     @ParameterizedTest
-    // @MethodSource("testDivisionInputParameters")
-    @MethodSource()
+    /*@CsvSource({
+            "6.2, 2, 3.1",
+            "71, 14, 5.07",
+            "18.3, 3.1, 5.90"
+    })
+    */
+
+    @CsvSource({"Senna, F1", "Keith Moon, ''"})
     void testDivision(double a, double b, double expected) {
 
         System.out.println("Test " + a + "/" + b + "=" + expected);
         Double actual = math.division(a, b);
 
-        assertEquals(expected, actual, 2D,() -> String.format("Expected %f but got %f", expected, actual));
+        assertEquals(expected, actual, 2D, () -> String.format("Expected %f but got %f", expected, actual));
 
     }
 
     // public static Stream<Arguments> testDivisionInputParameters() {
-    public static Stream<Arguments> testDivision() {
-        return  Stream.of(
-                Arguments.of(6.2D, 2D, 3.1D),
-                Arguments.of(71D, 14D, 5.07D),
-                Arguments.of(18.3D, 3.1D, 5.90D)
-        );
-    }
+    /** public static Stream<Arguments> testDivision() {
+     return  Stream.of(
+     Arguments.of(6.2D, 2D, 3.1D),
+     Arguments.of(71D, 14D, 5.07D),
+     Arguments.of(18.3D, 3.1D, 5.90D)
+     );
+     }
+     */
 
 
 }
