35f2fc497b165fe81f081bf02e7e87c0084df624
Coimbra <adsjkbd@gmail.com>
2024-06-13 05:51:18 -0300

    @ParameterizedTest com @MethodSource

---
+++ src/test/java/math/SimpleMathTestS4.java
@@ -0,0 +1,61 @@
+package math;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Test Math Operations in Simple Math Test")
+public class SimpleMathTestS4 {
+
+    SimpleMath math;
+
+    @BeforeAll
+    static void setup() {
+        System.out.println("Running @BeforeAll method!");
+    }
+
+    @AfterAll
+    static void cleanup() {
+        System.out.println("Running @AfterAll method!");
+    }
+
+    @BeforeEach
+    void beforeEachMethod() {
+        math = new SimpleMath();
+        System.out.println("Running @BeforeEach method!");
+    }
+
+    @AfterEach
+    void afterEachMethod() {
+        System.out.println("Running @AfterEach method!");
+    }
+
+    @DisplayName("Test 8.0 / 2.0 = 4.0")
+    @ParameterizedTest
+    // @MethodSource("testDivisionInputParameters")
+    @MethodSource()
+    void testDivision(double a, double b, double expected) {
+
+        System.out.println("Test " + a + "/" + b + "=" + expected);
+        Double actual = math.division(a, b);
+
+        assertEquals(expected, actual, 2D,() -> String.format("Expected %f but got %f", expected, actual));
+
+    }
+
+    // public static Stream<Arguments> testDivisionInputParameters() {
+    public static Stream<Arguments> testDivision() {
+        return  Stream.of(
+                Arguments.of(6.2D, 2D, 3.1D),
+                Arguments.of(71D, 14D, 5.07D),
+                Arguments.of(18.3D, 3.1D, 5.90D)
+        );
+    }
+
+
+}
