764bac40019da45bd087cdeb44293ab471bac5a9
Coimbra <adsjkbd@gmail.com>
2024-06-14 05:55:18 -0300

    @TestMethodOrder Ordenação Randômica

---
+++ src/test/java/math/MethodOrderedRandonlyTest.java
@@ -0,0 +1,29 @@
+package math;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(MethodOrderer.Random.class)
+public class MethodOrderedRandonlyTest {
+
+    @Test
+    void testA() {
+
+    }
+
+    @Test
+    void testB() {
+
+    }
+
+    @Test
+    void testC() {
+
+    }
+
+    @Test
+    void testD() {
+
+    }
+}
